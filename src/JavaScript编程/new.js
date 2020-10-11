2f99f50ede8c4cc1a677fa65eafef25c3f8cf9dd
hank.ren <987942918@qq.com>
2020-10-11 12:01:43 +0800

    feat(misc): add new problem

--- src/JavaScript编程/new.js
+++ src/JavaScript编程/new.js
@@ -3,3 +3,34 @@ export function _new(fn, ...args) {
   const res = fn.apply(obj, args);
   return res instanceof Object ? res : obj;
 }
+
+function multiRequest(urls, maxNum) {
+  const ret = [];
+  let i = 0;
+  let resolve;
+  const promise = new Promise(r => (resolve = r));
+  const addTask = () => {
+    if (i >= arr.length) {
+      return resolve();
+    }
+
+    const task = request(urls[i++]).finally(() => {
+      addTask();
+    });
+    ret.push(task);
+  };
+
+  while (i < maxNum) {
+    addTask();
+  }
+
+  return promise.then(() => Promise.all(ret));
+}
+
+// 模拟请求
+function request(url) {
+  return new Promise(r => {
+    const time = Math.random() * 1000;
+    setTimeout(() => r(url), time);
+  });
+}
--- src/add.js
+++ src/add.js
@@ -2,29 +2,37 @@ add(1)(2)(3) === 6;
 add(1, 2, 3)(4) === 10;
 add(1)(2)(3)(4)(5) === 15;
 
-function add(...args1) {
-  const args = args1;
-
-  function adder(...args2) {
-    args.push(args2);
-    return adder;
+/** 参数不定长柯里化 */
+function curry(fn, ...presetArgs) {
+  function curried(...resetArgs) {
+    const allArgs = [...presetArgs, ...resetArgs];
+    return curry.call(null, fn, ...allArgs);
   }
 
-  adder.toString = function() {
-    return args.reduce((a, b) => a + b);
+  curried.toString = () => {
+    return fn.apply(null, presetArgs);
   };
 
-  return adder;
+  return curried;
 }
 
-function curry(fn) {
-  const length = fn.length;
-  const getCurried = (params = []) => () => {
-    const args = params.concat([].slice.call(arguments));
-    if (args.length < length) {
-      getCurried(args);
+function dynamicAdd(...args) {
+  return args.reduce((prev, curr) => {
+    return prev + curr;
+  }, 0);
+}
+
+const add = curry(dynamicAdd);
+
+/** 参数定长柯里化 */
+function curry(fn, ...presetArgs) {
+  const argsLength = fn.length;
+  return function curried(...restArgs) {
+    const allArgs = [...presetArgs, ...restArgs];
+    if (allArgs >= argsLength) {
+      return fn.apply(this, allArgs);
+    } else {
+      return curry.call(null, fn, ...allArgs);
     }
-    return fn.apply(this, args);
   };
-  return getCurried();
 }
